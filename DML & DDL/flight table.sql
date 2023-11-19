33950b92cdbeae6f40f467454be1e805b110c29e
Gevorg Alaverdyan <57418717+gevorgalaverdyan@users.noreply.github.com>
2023-11-18 21:17:17 -0500

    Update and rename flight table.sql to flight_table.sql

--- DML & DDL/flight table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Flight (
-	flight_id INT PRIMARY KEY NOT NULL,
-	departure_location VARCHAR(100) NOT NULL,
-	arrival_location VARCHAR(100) NOT NULL,
-	departure_time DATE NOT NULL,
-	arrival_time DATE NOT NULL,
-	SNID INT,
-	FOREIGN KEY(SNID) REFERENCES Airplane(SNID) ON DELETE CASCADE
-);
