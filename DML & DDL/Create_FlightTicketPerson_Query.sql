5e5310dadaaafd92423b6cda119fe263c0a5dbd7
Gevorg Alaverdyan <57418717+gevorgalaverdyan@users.noreply.github.com>
2023-11-18 20:18:08 -0500

    Update Create_FlightTicketPerson_Query.sql

--- DML & DDL/Create_FlightTicketPerson_Query.sql
+++ DML & DDL/Create_FlightTicketPerson_Query.sql
@@ -1,6 +1,6 @@
 CREATE TABLE FlightTicketPerson(
     PRIMARY KEY(seat_num, flight_id, p_id),
-    seat_num INT,
+    seat_num INT NOT NULL,
     price FLOAT,
     class VARCHAR(50),
     Foreign Key (flight_id) REFERENCES (Flight) ON DELETE CASCADE,
